84ffd73eb33e6dfe35500b4437f9e2c2ed74db3f
jlight4 <60185932+jlight4@users.noreply.github.com>
2020-01-22 10:01:03 -0500

    First Comment

---
+++ Assignment 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMov = this.transform.position.x + speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        this.transform.position = new Vector3(xMov, this.transform.position.y, this.transform.position.z);
+    }
+}
